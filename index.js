9fe781e8a54a2a130328142c5325914bc2d886e5
Petrascu Ion <ionpetrascuc0318@gmail.com>
2025-02-07 09:25:33 +0200

    a

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+    res.json({
+        "message": "🦄🌈✨👋🌎🌍🌏✨🌈🦄"
+    });
+});
+
 app.get("/users", async (req, res) => {
     const users = await prisma.user.findMany();
     res.json(users);
